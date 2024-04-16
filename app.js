bb3a1f2d0ddde5da6ea6fcb7dab3be90d37fecc2
gaurav1954 <500090993@stu.upes.ac.in>
2024-04-16 13:04:47 +0530

    added contanerization scripts

--- app.js
+++ app.js
@@ -1,34 +1,37 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const User = require('./models/users')
-const Task = require('./models/task')
-const ejsMate = require('ejs-mate')
-const path = require('path')
-const all = require("./router/routes")
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const User = require('./models/users');
+const Task = require('./models/task');
+const ejsMate = require('ejs-mate');
+const path = require('path');
+const all = require("./router/routes");
 const flash = require('express-flash');
-mongoose.connect('mongodb://127.0.0.1:27017/task-management')
+
+// Connect to MongoDB using the MONGO_URL environment variable
+mongoose.connect(process.env.MONGO_URL)
     .then(() => {
-        console.log("sucessful  connection")
+        console.log("Successful connection to MongoDB");
     })
     .catch((err) => {
-        console.log("failed mongo connection", err)
-    })
-app.engine('ejs', ejsMate)
-app.set('view engine', 'ejs')
-app.set("views", path.join(__dirname, "views"))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname, '/public')))
-const session = require('express-session')
+        console.error("Failed to connect to MongoDB:", err);
+    });
+
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/public')));
+const session = require('express-session');
 
 app.use(session({
     secret: 'thisIsJustDumb',
     saveUninitialized: false,
-    resave: false   //this is like signing the cookie
-}))
+    resave: false   // This is like signing the cookie
+}));
 app.use(flash());
-app.use('/', all)
+app.use('/', all);
 
 app.listen(8000, () => {
-    console.log("App up and running")
-})
+    console.log("App up and running on port 8000");
+});
